8ffcc027f72169f5a54b122eb2c845aab21a00c1
sandeepa <sanvidurath98@gmail.com>
2025-04-01 17:40:57 +0530

    added navbar

---
+++ FrontEnd/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './common/navbar/navbar.component';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [NavbarComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'FrontEnd';
+}
